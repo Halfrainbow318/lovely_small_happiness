b00e05a9e12035d036dcb6ad0254d20682698c4e
Halfrainbow318 <ohsi_0615@naver.com>
2020-07-20 16:29:27 +0900

    upload

---
+++ Assets/Scripts/ScreenMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenMove : MonoBehaviour
+{
+    public void To01()
+    {
+        SceneManager.LoadScene("01_MainTitle");
+    }
+
+    public void To02()
+    {
+        SceneManager.LoadScene("02_Newgame");
+    }
+
+    public void To06()
+    {
+        SceneManager.LoadScene("06_Setting");
+    }
+
+    public void To07()
+    {
+        SceneManager.LoadScene("07_Talk");
+    }
+
+    public void Toexit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/TalkManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class TalkManager : MonoBehaviour
+{
+    public Text text;
+
+    private bool talking = true;
+
+    private List<string> scenario;
+    private int Count = 0;
+
+    public int day = 0;
+
+    string FilePath;
+    string FileName;
+
+    private void Start()
+    {
+        FileName = "day" + day + ".txt";
+        FilePath = @"C:\Users\ohsei\Desktop\ㅇㅅㅇ\문서들\대학\여름_학기\동아리\lovely_small_happiness\Assets\texts\" + FileName;
+
+        scenario = new List<string>();
+
+        ReadFile();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            text.text = scenario[Count];
+            Count++;
+        }
+    }
+
+    public void ReadFile()
+    {
+        scenario = File.ReadAllLines(FilePath);
+        foreach (string line in scenario)
+        {
+            scenario.Add(line.ToString());
+        }
+    }
+}
