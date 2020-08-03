3a9d3d5d50e3ff8a63c2d5a41ab0fdfda5dbde9f
Halfrainbow318 <ohsi_0615@naver.com>
2020-08-03 10:42:04 +0900

    talk

--- Assets/Scripts/TalkManager.cs
+++ Assets/Scripts/TalkManager.cs
@@ -12,13 +12,15 @@ public class TalkManager : MonoBehaviour
     public SpriteRenderer ctrRender; //캐릭터 스트라이트 렌더
     private Sprite ctrUI; //캐릭터 스트라이트를 담는 이미지를 담는 공간
 
-    private int day = 2; //텍스트 파일을 구분하기 위한 날짜 변수
+    public GameObject starttominigame;
+
+    private int day = 4; //텍스트 파일을 구분하기 위한 날짜 변수
     private bool IsTalk = false; //대화 상태를 나타내는 변수
 
     List<string> TextFile = new List<string>(); //텍스트 파일을 한줄 씩 저장할 리스트
     string TextRead; //출력할 대사를 담는 변수
     int num = 0; //한줄씩 읽는 변수
-    int max = 0; //줄 갯수
+    int max = 0; //현재 읽고 있는 파일의 줄 갯수
 
     public List<Talk> TextData; //Talk클래스 리스트 호출
     Talk npcdata = new Talk(); //Talk클래스를 사용하는 목록
@@ -40,32 +42,33 @@ public class TalkManager : MonoBehaviour
         Spacebar();
     }
 
-    private void ReadFile()
+    private void ReadFile() //파일을 불러와 한줄 씩 저장하는 함수
     {
         TextAsset scenario = Resources.Load("day" + day.ToString()) as TextAsset; //파일을 불러오고 텍스트를 시나리오 텍스트 에셋에 넣기
 
         StringReader TextReader = new StringReader(scenario.text); //StringReader 선언
 
-        while (TextReader != null) //비어있는 줄이 나올 때 까지
+        while (TextReader != null) //비어있는 줄이 나올 때 까지 (파일의 맨 끝 줄에 도착할 때 까지)
         {
             TextRead = TextReader.ReadLine(); //파일 한줄 읽기
-            max++;
-            TextFile.Add(TextRead);
+            max++; //현재 읽고 있는 파일의 마지막 줄 수가 한줄 증가
+            TextFile.Add(TextRead); //저장용 리스트에 TextReader로 읽은 문자열 한줄을 추가
 
-            if (string.IsNullOrEmpty(TextRead) == true) //비어있는 줄이 나온다면
+            if (string.IsNullOrEmpty(TextRead) == true) //만약 비어있는 줄이 나온다면 (파일의 맨 끝 줄에 도착한다면)
             {
                 TextReader.Close(); //사용 후 닫기
-                break; //브레이크
+                break; //읽기를 그만하고 나오기
             }
         }
     }
 
-    public void Spacebar()
+    public void Spacebar() //스페이스바를 누를 때 마다 한줄 씩 대사를 출력함
     {
         if (Input.GetKeyDown(KeyCode.Space)) //스페이스바를 눌렀을 때
         {
-            TextRead = TextFile[num];
+            TextRead = TextFile[num]; //저장용 리스트의 num번째줄을 불러와 문자형 변수에 저장함
 
+            //TextRead를 ':'을 이용해 나누고 각각의 Talk(라고 이름을 붙인) 클래스의 변수에 저장
             npcdata.name = TextRead.Split(':')[0]; //대사하는 사람의 이름
             npcdata.talk = TextRead.Split(':')[1]; //대사
             npcdata.ctr = int.Parse(TextRead.Split(':')[2]); //대사하는 사람의 스프라이트
@@ -78,14 +81,13 @@ public class TalkManager : MonoBehaviour
             ctrRender.sprite = Resources.Load("ctr" + npcdata.ctr.ToString(), typeof(Sprite)) as Sprite; //스프라이트 변경
             ctrRender.color = new Color(npcdata.col, npcdata.col, npcdata.col, npcdata.alp); //색상, 투명도 설정
 
-            Debug.Log(num);
-            num++;
-            if (num == max)
+            num++; //num+1을 해주어 다음 줄을 읽을 수 있게함
+            if (num == max) //만약 현재 읽고 있는 줄이 마지막 줄이라면
             {
-                IsTalk = false; //대화 종료를 알림
-                Debug.Log("대화종료");
+                starttominigame.SetActive(true);
+                IsTalk = false; //대화를 비활성화함
+                Debug.Log("대화종료"); //대화 종료를 알림
             }
         }
     }
-
 }
--- Assets/Scripts/minigame1.cs
+++ Assets/Scripts/minigame1.cs
@@ -26,19 +26,21 @@ public class minigame1 : MonoBehaviour
     {
         startbutton.text = " 현재 호감도 : " + luv.ToString();
     }
-    private void Update()
+    private void Update() //타이머를 만들기 위해 Update함수에 넣음
     {
-        if (start == true)
+        if (start == true) //게임을 시작 했다면
         {
-            if (time > 0)
-                time -= Time.deltaTime;
-
-            timeText.text = Mathf.Ceil(time).ToString();
-
-            if (time <=0)
+            if (time > 0) //시간이 0이 아니라면
             {
-                minigame1_end.SetActive(true);
-                start = false;
+                time -= Time.deltaTime; //시간을 감소시킴
+            }
+
+            timeText.text = Mathf.Ceil(time).ToString(); //시간을 정수로 올림하여 변환함
+
+            if (time <= 0) //시간이 다 되었다면
+            {
+                minigame1_end.SetActive(true); //게임 종료 창 활성화
+                start = false; //게임 종료
                 Result1.text = "책을 고르지 못했습니다.";
                 pm = 5;
                 Result2.text = "호감도가 " + pm + "만큼 감소합니다...";
@@ -46,7 +48,6 @@ public class minigame1 : MonoBehaviour
                 Result3.text = "현재 호감도 : " + luv.ToString();
             }
         }
-
     }
 
     public void Gamestart()
@@ -54,19 +55,21 @@ public class minigame1 : MonoBehaviour
         start = true;
     }
 
-    public void Best()
+    public void Best() //미니게임을 성공적으로 완료하였을 경우
     {
-        start = false;
+        minigame1_end.SetActive(true); //게임 종료 창 활성화
+        start = false; //게임 종료
         Result1.text = "정확한 책을 골랐습니다!";
-        pm = 10;
+        pm = 10; 
         Result2.text = "호감도가 " + pm + "만큼 증가합니다!";
-        luv += pm;
+        luv += pm; 
         Result3.text = "현재 호감도 : " + luv.ToString();
     }
 
-    public void Good()
+    public void Good() //미니게임을 적당히 완료한 경우
     {
-        start = false;
+        minigame1_end.SetActive(true); //게임 종료 창 활성화
+        start = false; //게임 종료
         Result1.text = "정확하지는 않지만, 그래도 비슷한 책을 골랐네요.";
         pm = 0;
         Result2.text = "호감도가 변하지 않습니다.";
@@ -74,9 +77,10 @@ public class minigame1 : MonoBehaviour
         Result3.text = "현재 호감도 : " + luv.ToString();
     }
 
-    public void Fail()
+    public void Fail() //미니게임을 실패하였을 경우
     {
-        start = false;
+        minigame1_end.SetActive(true); //게임 종료 창 활성화
+        start = false; //게임 종료
         Result1.text = "다른 책을 골랐습니다.";
         pm = 5;
         Result2.text = "호감도가 " + pm + "만큼 감소합니다...";
